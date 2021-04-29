3f160f8e64303e9aa28d76ff9abdfddb797ee24b
Santa Chakraborty <chakraborty.santa@gmail.com>
2021-04-28 22:31:40 -0400

    final commit

--- MOD1-Game/app.js
+++ MOD1-Game/app.js
@@ -2,126 +2,166 @@
 // alert("hello")
 
 
-const obox_text="0";
+const obox_text="O";
 const xbox_text="X";
-let current_player= obox_text;
+let current_player= xbox_text;
+
 let Player1, Player2;
-// let winner;
-let empty_boxes=[];
+// let empty_boxes=[];
+let empty_array=[];
 let count=0;
+var counterPlayer1=0;
+var counterPlayer2=0;
 
 let gameOver=false;
 
-
-
-
 function data(event){
       
-  if(gameOver){
-    console.log('gameOver');
-    return;
-    
-  }
-      console.log("box clicked");  //just for checking purpose on console
-      
-      const id=event.target.id;   // storing index nos for every call
-      console.log(id);
+    if(gameOver){     
+       console.log('gameOver');
+       return;
+    }
+    console.log("box clicked");  
+    const id=event.target.id;   // storing index nos for every call
+    console.log(id);
 
-      if(!empty_boxes[id]){
+    if(!empty_array[id]){
       
-         empty_boxes[id]=current_player;    //updating the value of empty_spaces array with current_player value
-         event.target.innerHTML = current_player;  //updating the box with current player
-         count++;
-         //setting the new value for current player, but we need to check first the current player is winner or not
+          empty_array[id]=current_player;    //updating the value of empty_spaces array with current_player value
+          event.target.innerHTML = current_player;  //updating the box with current player
+          count++;                 
+          
 
-           if(playerHasWon()){
-              if(current_player === obox_text){
-                 document.getElementById("status").innerHTML='Player1 is the winner';
+          if(playerHasWon()){
+              if(current_player === xbox_text){
+                  counterPlayer1++;
+                  document.getElementById("status").innerHTML='Player 1 is the winner';
+                  document.getElementById("player1").innerHTML='Player 1 scored:'+ counterPlayer1;
+                  gameOver=true;
+                  return;
+               }else{
+                 counterPlayer2++;
+                 document.getElementById("status").innerHTML='Player 2 is the winner';
+                 document.getElementById("player2").innerHTML='Player 2 scored:'+ counterPlayer2;
                  gameOver=true;
                  return;
-              }else{
-                 document.getElementById("status").innerHTML='Player2 is the winner';
-                 gameOver=true;
-                 return;
-              }
-              
-           }
-
-           if(current_player === obox_text){
-                current_player = xbox_text;
-           }else{
-              current_player = obox_text;
-           }
+               }
+          }
+          if(current_player === xbox_text){
+                current_player = obox_text;
+          }else{
+              current_player = xbox_text;
+          }
 
           if(count===9){
-              document.getElementById("status").innerHTML='Draw';
+              document.getElementById("status").innerHTML='Tie';
               gameOver=true;
           }
       }
-  };
+  }
    
-  function playerHasWon(){
+   function playerHasWon(){
       //checking with 0th element
-      if(empty_boxes[0]===current_player){
-         if(empty_boxes[1] ===current_player && empty_boxes[2] ===current_player){
-           console.log(`${current_player} winner`);
-           return true;
+      if(empty_array[0]===current_player){
+          if(empty_array[1] ===current_player && empty_array[2] ===current_player){
+
+             document.getElementById("0").style.color="red";
+             document.getElementById("1").style.color="red";
+             document.getElementById("2").style.color="red";
+             console.log(`${current_player} winner`);
+             return true;
           }
-         if(empty_boxes[3] ===current_player && empty_boxes[6] ===current_player){
+          if(empty_array[3] ===current_player && empty_array[6] ===current_player){
+
+             document.getElementById("0").style.color="red";
+             document.getElementById("3").style.color="red";
+             document.getElementById("6").style.color="red";
+             console.log(`${current_player} winner`);
+             return true;
+          }
+          if(empty_array[4] ===current_player && empty_array[8] ===current_player){
+
+             document.getElementById("0").style.color="red";
+             document.getElementById("4").style.color="red";
+             document.getElementById("8").style.color="red";
+             console.log(`${current_player} winner`);
+             return true;
+          }
+      }
+     
+      if(empty_array[4] === current_player){
+         if(empty_array[1] === current_player && empty_array[7] === current_player){
+
+            document.getElementById("4").style.color="red";
+            document.getElementById("1").style.color="red";
+            document.getElementById("7").style.color="red";
             console.log(`${current_player} winner`);
-            return true;
+             return true;
          }
-         if(empty_boxes[4] ===current_player && empty_boxes[8] ===current_player){
+         if(empty_array[3] === current_player && empty_array[5] === current_player){
+
+             document.getElementById("4").style.color="red";
+             document.getElementById("3").style.color="red";
+             document.getElementById("5").style.color="red";
              console.log(`${current_player} winner`);
              return true;
          }
-      }
-     //checking with index 4th element
-      if(empty_boxes[4] === current_player){
-         if(empty_boxes[1] === current_player && empty_boxes[7] === current_player){
-            console.log(`${current_player} winner`);
-             return true;
-         }
-         if(empty_boxes[3] === current_player && empty_boxes[5] === current_player){
-            console.log(`${current_player} winner`);
-            return true;
-         }
-         if(empty_boxes[2] === current_player && empty_boxes[6] === current_player){
+         if(empty_array[2] === current_player && empty_array[6] === current_player){
+
+             document.getElementById("4").style.color="red";
+             document.getElementById("2").style.color="red";
+             document.getElementById("6").style.color="red";
              console.log(`${current_player} winner`);
              return true;
          }
       }
       //checking with index 8th element
-      if(empty_boxes[8] === current_player){
-         if(empty_boxes[6] === current_player && empty_boxes[7] === current_player){
-            console.log(`${current_player} winner`);
-            return true;
-         }
-         if(empty_boxes[2] === current_player && empty_boxes[5] === current_player){
+      if(empty_array[8] === current_player){
+          if(empty_array[6] === current_player && empty_array[7] === current_player){
+
+             document.getElementById("8").style.color="red";
+             document.getElementById("6").style.color="red";
+             document.getElementById("7").style.color="red";
              console.log(`${current_player} winner`);
              return true;
-         }
+          }
+          if(empty_array[2] === current_player && empty_array[5] === current_player){
+
+              document.getElementById("8").style.color="red";
+              document.getElementById("2").style.color="red";
+              document.getElementById("5").style.color="red";
+              console.log(`${current_player} winner`);
+              return true;
+          }
+       }
+   }
+
+
+ function restartGame(){
+      count=0;
+
+      let boxes=document.getElementsByClassName("box");
+      for(let i=0; i<boxes.length; i++){
+          boxes[i].innerHTML='';
+          boxes[i].style.color="black";
       }
-  };
-
-
-function restartGame(){
-   count=0;
-  //  empty_boxes.forEach(box => {box.innerHTML=''})
-
-  let boxes=document.getElementsByClassName("box");
-  for(let i=0; i<boxes.length; i++){
-    boxes[i].innerHTML='';
-  }
-   console.log(empty_boxes);
-   empty_boxes=[];
-   gameOver=false;
+      // console.log(empty_array);
+       empty_array=[];
+       gameOver=false;
   
-   
-  document.getElementById("status").innerHTML='Play';
-   current_player=obox_text;
+       document.getElementById("status").innerHTML="Let's Play";
+       current_player=xbox_text;
       
-}
-document.getElementById("restart-btn").addEventListener("click",restartGame);
-restartGame();
+  }
+
+   function resetScore(){
    
+        counterPlayer1=0;
+        counterPlayer2=0;
+        console.log(counterPlayer1);
+        console.log(counterPlayer2);
+        document.getElementById("player1").innerHTML='Player 1 scored:'+ counterPlayer1;
+        document.getElementById("player2").innerHTML='Player 2 scored:'+ counterPlayer2;
+}
+
+
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # MOD1-Game
+Game: Tic Tac Toe
+
+Rules: 2 player game. The winner has to get 3 X's or 3 O's in a row, column or diagonally. 
+Tools used: HTML5, CSS3, Vanilla Javascript.
+Design: 3X3 grid
+Description of game: Taking turns click only on empty spaces to mark X's and O's in the grid.
+Restart button will clear the grid and start over again. Reset will clear the score for players.
+
+Technical details: First, game will start with current player X. Click event will update an empty
+array, grid boxes with that current player, and will check all the possibilities to win.Second 
+player will be updated with O and continues.
+
+Technical challenges: Faced some challenges to stop the game even after finding the winner.Had to
+give effort to function the Reset button properly.
